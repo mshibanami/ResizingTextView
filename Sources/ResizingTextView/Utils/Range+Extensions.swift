a1f03b13fdaa8f938e39605fcd9d77bab38d6af3
Manabu Nakazawa <mshibanami+git@gmail.com>
2025-05-16 13:44:12 +1000

    Fix a crash

--- Sources/ResizingTextView/Utils/Range+Extensions.swift
+++ Sources/ResizingTextView/Utils/Range+Extensions.swift
@@ -2,11 +2,7 @@
 
 extension Range where Bound == String.Index {
     func isValid(in string: String) -> Bool {
-        guard let lower16 = lowerBound.samePosition(in: string.utf16),
-              let upper16 = upperBound.samePosition(in: string.utf16) else {
-            return false
-        }
-        return lower16 >= string.utf16.startIndex
-            && upper16 <= string.utf16.endIndex
+        lowerBound >= string.startIndex &&
+        upperBound <= string.endIndex
     }
 }
